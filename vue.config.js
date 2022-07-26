032fae843fdb701c833ec31c6bc46e498061f404
Bill-Yeh <lawyer112240@gmail.com>
2022-07-26 17:21:08 +0800

    加上'/'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/google_map',
+  publicPath: '/google_map/',
 })
 
 module.exports = {
