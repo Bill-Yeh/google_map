34c6cbfbf20d3f6d74584ab168a3eb5e71f24777
Bill-Yeh <lawyer112240@gmail.com>
2022-07-26 17:09:21 +0800

    修改路徑:google_map，並把gitignore /dist刪除

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  publicPath: '/google_map/dist',
 })
 
 module.exports = {
